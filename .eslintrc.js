1b4a7b02d1f50a3acaf32ae232589733d3393a71
RayBob1993 <dimaxfallenangel@yandex.ru>
2021-05-29 18:33:31 +0300

    Разработка основы

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,40 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es6: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaVersion: 6,
+    sourceType: 'module'
   },
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
   rules: {
-    semi: [
-      2,
-      'always',
+    'vue/html-indent': ['error', 2, {
+      attribute: 1,
+      baseIndent: 1,
+      closeBracket: 0,
+      alignAttributesVertically: true
+    }],
+    'vue/script-indent': ['error', 2, {
+      baseIndent: 1
+    }],
+    semi: [2, 'always',
       {
         omitLastInOneLineBlock: false
       }
     ]
-  }
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off'
+      }
+    }
+  ]
 };
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="container">
-
+    <input
+      type="text"
+      autofocus
+      autocomplete="off"
+      placeholder="sdfvsdfv"
+      required
+    >
   </div>
 </template>
 
 <script>
   export default {
-    name: 'PageMain'
-  }
+    name: 'PageMain',
+    data: () => ({
+
+    }),
+    created () {
+    },
+    mounted () {
+    },
+    methods: {
+
+    }
+  };
 </script>
