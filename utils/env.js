a0a670ca55465cf297030248eaf9b6ee360046c8
Turkov Dmitry <dimaxfallenangel@yandex.ru>
2021-12-09 20:37:14 +0300

    Update env.js

--- utils/env.js
+++ utils/env.js
@@ -1,7 +1,10 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
 const isProduction = process.env.NODE_ENV === 'production';
 
+const envToBoolean = env => Boolean(env && env.toLowerCase() !== 'false' && env !== '0');
+
 export {
   isDevelopment,
-  isProduction
+  isProduction,
+  envToBoolean
 };
