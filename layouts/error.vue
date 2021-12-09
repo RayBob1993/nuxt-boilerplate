0dc59a40cbed0f508ddd924402c2501db34853f2
Turkov Dmitry <dimaxfallenangel@yandex.ru>
2021-12-09 20:42:26 +0300

    Create error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ error.statusCode }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PageError',
+    props: {
+      error: {
+        type: Object,
+        default: () => ({})
+      }
+    }
+  };
+</script>
